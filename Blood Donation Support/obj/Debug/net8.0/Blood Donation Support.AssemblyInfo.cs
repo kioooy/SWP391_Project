7ea8e6a2ccce65b130331e09085140ad75ce1ba6
Booo <nguyendat7121@gmail.com>
2025-06-22 11:58:35 +0700

    Commit

--- Blood Donation Support/obj/Debug/net8.0/Blood Donation Support.AssemblyInfo.cs
+++ Blood Donation Support/obj/Debug/net8.0/Blood Donation Support.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blood Donation Support")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b40147da453ac4b1379a926b057e6412c63c94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138eb369abb19a2814f0777fcf6f9b5993175ee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blood Donation Support")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blood Donation Support")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

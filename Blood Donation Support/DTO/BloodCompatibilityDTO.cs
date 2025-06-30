5d74c7e095a35990c7e68d63e8f5f1163b8ffc5c
tinbusiness-work <tinbusiness.work@gmail.com>
2025-06-30 14:06:30 +0700

    Update DTO

---
+++ Blood Donation Support/DTO/BloodCompatibilityDTO.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Blood_Donation_Support.DTO
+{
+    public class BloodCompatibilityRequestDto
+    {
+        [Required]
+        public string RecipientBloodType { get; set; } // e.g., "A+", "O-"
+
+        // "whole-blood", "red-cell", "plasma", "platelet"
+        public string Component { get; set; } = "whole-blood";
+    }
+
+    public class BloodCompatibilityResponseDto
+    {
+        public List<string> CompatibleBloodTypes { get; set; }
+    }
+
+}
---
+++ Blood Donation Support/DTO/HospitalDTO.cs
@@ -0,0 +1,9 @@
+namespace Blood_Donation_Support.DTO
+{
+    public class HospitalLocationUpdateDTO
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+}

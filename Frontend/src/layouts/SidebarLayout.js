56a3ad5474807bdf5faaa5af4a1e1bd5e2be6ae6
Booo <nguyendat7121@gmail.com>
2025-07-04 21:15:05 +0700

    Update UI Staff/Admin

--- Frontend/src/layouts/SidebarLayout.js
+++ Frontend/src/layouts/SidebarLayout.js
@@ -26,7 +26,6 @@ const SidebarLayout = () => {
     { path: "/manage-requests", label: "Yêu cầu hiến máu", icon: <AssignmentIcon /> },
     { path: "/manage-blood-periods", label: "Đợt hiến máu", icon: <BloodtypeIcon /> },
     { path: "/blood-inventory", label: "Kho máu tổng hợp", icon: <BloodtypeIcon /> },
-    { path: "/manage-blood-storage", label: "Kho máu chi tiết", icon: <BloodtypeIcon /> },
     { path: "/transfusion-management", label: "Truyền máu", icon: <LocalHospitalIcon /> },
     { path: "/manage-article", label: "Tài liệu", icon: <MenuBookIcon /> },
     { path: "/manage-blog", label: "Blog", icon: <EditNoteIcon /> },
